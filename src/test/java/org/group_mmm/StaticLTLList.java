b7a5da71de1eb7bee60bd80edd0bd3c86f1c4ae7
Masaki Waga <masakiwaga@gmail.com>
2021-04-01 17:27:45 +0900

    fixed the test

--- src/test/java/org/group_mmm/StaticLTLList.java
+++ src/test/java/org/group_mmm/StaticLTLList.java
@@ -1,20 +1,17 @@
 package org.group_mmm;
 
-import de.learnlib.api.oracle.MembershipOracle;
 import de.learnlib.api.oracle.PropertyOracle;
-import de.learnlib.api.query.DefaultQuery;
+import de.learnlib.oracle.property.MealyFinitePropertyOracle;
 import lombok.AllArgsConstructor;
-import net.automatalib.automata.transducers.MealyMachine;
-import net.automatalib.words.Word;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import lombok.extern.slf4j.Slf4j;
 
-import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
 
+@Slf4j
 @AllArgsConstructor
-class StaticLTLList implements AdaptiveSTLUpdater {
+class StaticLTLList extends AbstractAdaptiveSTLUpdater {
     List<String> ltlProperties;
 
     @Override
@@ -28,33 +25,17 @@ class StaticLTLList implements AdaptiveSTLUpdater {
     }
 
     @Override
-    public List<PropertyOracle.MealyPropertyOracle<String, String, String>> list() {
-        return null;
+    public int size() {
+        return ltlProperties.size();
     }
 
     @Override
     public Stream<PropertyOracle.MealyPropertyOracle<String, String, String>> stream() {
-        return null;
-    }
-
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public void setMemOracle(MembershipOracle.@NotNull MealyMembershipOracle<String, String> memOracle) {
-
-    }
-
-    @Override
-    public List<PropertyOracle<String, ? super MealyMachine<?, String, ?, String>, ?, Word<String>>> getPropertyOracles() {
-        return null;
-    }
-
-    @Nullable
-    @Override
-    public DefaultQuery<String, Word<String>> findCounterExample(@NotNull MealyMachine<?, String, ?, String> objects, @NotNull Collection<? extends String> collection) {
-        return null;
+        if (Objects.isNull(inclusionOracle) || Objects.isNull(emptinessOracle)) {
+            log.error("AbstractAdaptiveSTLUpdater::stream is called before setting inclusionOracle or emptinessOracle");
+            throw new NullPointerException();
+        }
+        return this.getLTLProperties().stream().map(ltl ->
+                new MealyFinitePropertyOracle<>(ltl, inclusionOracle, emptinessOracle, modelChecker));
     }
 }
