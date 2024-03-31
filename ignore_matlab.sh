456dfe4e802cefe38fa3dc81af85160cf537beed
Masaki Waga <masakiwaga@gmail.com>
2024-03-31 18:35:24 +0900

    Updated the list of the ignored files

--- ignore_matlab.sh
+++ ignore_matlab.sh
@@ -16,26 +16,12 @@ awk '/BEGIN MATLAB/{ignore=1}ignore==0;/END MATLAB/{ignore=0}' pom.xml > "$TMPFI
 mv "$TMPFILE" pom.xml
 
 # remove matlab related files
-rm -f ./src/main/java/org/group_mmm/SimulinkMembershipOracle.java 
-rm -f ./src/main/java/org/group_mmm/SimulinkMembershipOracleCost.java 
 rm -f ./src/main/java/org/group_mmm/SimulinkSUL.java 
-rm -f ./src/main/java/org/group_mmm/SimulinkVerifier.java 
+rm -f ./src/main/java/org/group_mmm/SimulinkSULVerifier.java 
 rm -f ./src/main/java/org/group_mmm/FalCAuN.java
-rm -f ./src/main/java/org/group_mmm/AbstractSelectEQOracle.java
-rm -f ./src/main/java/org/group_mmm/GAEQOracle.java
-rm -f ./src/main/java/org/group_mmm/HillClimbingEQOracle.java
-rm -f ./src/main/java/org/group_mmm/MutateSelectEQOracle.java
-rm -f ./src/main/java/org/group_mmm/SAEQOracle.java
-rm -f ./src/main/java/org/group_mmm/SimulinkMembershipOracle.java
-rm -f ./src/main/java/org/group_mmm/SimulinkMembershipOracleCost.java
-rm -f ./src/main/java/org/group_mmm/EQSearchProblem.java
-rm -f ./src/main/java/org/group_mmm/SimulinkGenerationalGeneticAlgorithm.java
-rm -f ./src/main/java/org/group_mmm/SimulinkSteadyStateGeneticAlgorithm.java
 rm -f ./src/main/java/org/group_mmm/SimulinkRandomTester.java
 
-rm -f ./src/test/java/org/group_mmm/SimulinkSULMapperTest.java
 rm -f ./src/test/java/org/group_mmm/SimulinkSULTest.java
-rm -f ./src/test/java/org/group_mmm/SimulinkVerifierTest.java
-rm -f ./src/test/java/org/group_mmm/SimulinkMembershipOracleTest.java
+rm -f ./src/test/java/org/group_mmm/SimulinkSULVerifierTest.java
+rm -f ./src/test/java/org/group_mmm/NumericMembershipOracleTest.java
 rm -f ./src/test/java/org/group_mmm/AutotransExample*
-rm -f ./src/test/java/org/group_mmm/HillClimbingEQOracleTest.java
