79e42b91eb83afec73caf92d99b458935689c706
taiseiKMC <taisei@kmc.gr.jp>
2025-02-03 22:53:41 +0900

    Add PythonNumericSUL, PythonContinuousNumericSUL
    * Move pre, post, step, and close PyCallable object to PythonModel

---
+++ python-wrapper/src/main/java/net/maswag/falcaun/PythonContinuousNumericSUL.java
@@ -0,0 +1,170 @@
+package net.maswag.falcaun;
+
+import de.learnlib.sul.SUL;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import net.automatalib.word.Word;
+import net.automatalib.word.WordBuilder;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import java.io.Closeable;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * The System Under Learning implemented by a Simulink. We use the fixed step
+ * execution of Simulink to make sampling easier.
+ */
+@Slf4j
+public class PythonContinuousNumericSUL implements ContinuousNumericSUL, Closeable {
+    /**
+     * The signal step of the input signal.
+     */
+    private final Double signalStep;
+    ArrayList<List<Double>> outputSignals = new ArrayList<List<Double>>();
+    private final PythonModel model;
+    private final TimeMeasure simulationTime = new TimeMeasure();
+
+    @Getter
+    private int counter = 0;
+
+    public PythonContinuousNumericSUL(String initScript, Double signalStep)
+            throws InterruptedException, ExecutionException {
+        this.model = new PythonModel(initScript);
+        this.signalStep = signalStep;
+    }
+
+    private Signal inputSignal;
+
+    /**
+     * The current time of the simulation
+     */
+    public double getCurrentTime() {
+        return inputSignal.duration();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean canFork() {
+        return false;
+    }
+
+    /**
+     * Clear the counter and the time measure.
+     */
+    @Override
+    public void clear() {
+        simulationTime.reset();
+        counter = 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void pre() {
+        inputSignal = new Signal(signalStep);
+        this.model.pre();
+        counter++;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void post() {
+        this.model.post();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nullable
+    @Override
+    public ExtendedIOSignalPiece<List<Double>> step(@Nullable List<Double> inputSignal) {
+        if (inputSignal == null) {
+            return null;
+        }
+        this.inputSignal.add(inputSignal);
+
+        simulationTime.start();
+        List<Double> outputSignal = this.model.step(inputSignal);
+        simulationTime.stop();
+
+        this.outputSignals.add(outputSignal);
+        return new ExtendedIOSignalPiece<>(inputSignal, outputSignal, outputSignals);
+
+    }
+
+    /**
+     * Execute the Simulink model by feeding inputSignal
+     * <p>
+     * For inputSignal = a1, a2, ..., an, we construct a timed word w = (a1, 0),
+     * (a2, T), (a3, 2 * T), ... (an, (n - 1) * T) and execute the Simulink model by
+     * feeding the piecewise-linear interpolation of w.
+     *
+     * @param inputSignal The input signal
+     * @return The output signal. The size is same as the input.
+     */
+    @Override
+    public IOContinuousSignal<List<Double>> execute(Word<List<Double>> inputSignal)
+            throws InterruptedException, ExecutionException {
+        pre();
+
+        ArrayList<List<Double>> outputs = new ArrayList<List<Double>>();
+        ArrayList<Double> timestamps = new ArrayList<Double>();
+        ArrayList<Double> ret;
+
+        simulationTime.start();
+        for (var e : inputSignal) {
+            this.inputSignal.add(e);
+
+            try {
+                ret = this.model.step(e);
+            } catch (Exception exc) {
+                throw new InterruptedException(exc.toString());
+            }
+
+            outputs.add(ret);
+            timestamps.add(this.getCurrentTime());
+        }
+        simulationTime.stop();
+
+        ValueWithTime<List<Double>> values = new ValueWithTime<>(timestamps, outputs);
+        WordBuilder<List<Double>> builder = new WordBuilder<>();
+        for (int i = 0; i < inputSignal.size(); i++) {
+            builder.add(values.at(i * this.signalStep));
+        }
+        return new IOContinuousSignal<>(inputSignal, builder.toWord(), values, this.signalStep);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public SUL<List<Double>, IOSignalPiece<List<Double>>> fork() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Close the MATLAB engine. This method must be called when the object is no
+     * longer used.
+     */
+    @Override
+    public void close() {
+        this.model.close();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public double getSimulationTimeSecond() {
+        return this.simulationTime.getSecond();
+    }
+}
--- python-wrapper/src/main/java/net/maswag/falcaun/PythonModel.java
+++ python-wrapper/src/main/java/net/maswag/falcaun/PythonModel.java
@@ -5,138 +5,57 @@ import jep.JepException;
 import jep.SharedInterpreter;
 import jep.python.PyCallable;
 import jep.python.PyObject;
-import lombok.Getter;
-import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.extern.slf4j.Slf4j;
-import net.automatalib.word.Word;
-import org.apache.commons.lang3.ArrayUtils;
-//import org.junit.rules.ExternalResource;
-
-import javax.annotation.Nonnull;
-
-import java.io.OutputStream;
-import java.util.*;
-//import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
 
 /**
- * Raw Simulink model. We use the fixed step execution of Simulink to make sampling easier.
+ * Raw Simulink model. We use the fixed step execution of Simulink to make
+ * sampling easier.
  */
 @Slf4j
 public class PythonModel {
-    /**
-     * The signal step of the input signal.
-     */
-    @Getter
-    private final Double signalStep;
-
-    /**
-     * The current time of the simulation
-     */
-    public double getCurrentTime() {
-        return inputSignal.duration();
-    }
-    private Signal inputSignal;
-    @Getter
-    private int counter = 0;
-    private final TimeMeasure simulationTime = new TimeMeasure();
-
-
     private final SharedInterpreter interpreter;
-    private final PyObject pysul;
-    public PythonModel(String initScript, Double signalStep) throws JepException {
+
+    private final PyCallable pyPre, pyPost, pyStep, pyClose;
+
+    public PythonModel(String initScript) throws JepException {
         SharedInterpreter.setConfig(new JepConfig().redirectStdout(System.out).redirectStdErr(System.err));
         this.interpreter = new SharedInterpreter();
-        this.signalStep = signalStep;
 
         this.interpreter.runScript(initScript);
-        //this.interpreter.eval(String.format("import %s as s", moduleName));
+        // this.interpreter.eval(String.format("import %s as s", moduleName));
         this.interpreter.eval("sul = SUL()");
-        this.pysul = this.interpreter.getValue("sul", PyObject.class);
+        var pysul = this.interpreter.getValue("sul", PyObject.class);
 
-
-        // Initialize the current state
-        this.reset();
+        this.pyPre = pysul.getAttr("pre", PyCallable.class);
+        this.pyPost = pysul.getAttr("post", PyCallable.class);
+        this.pyStep = pysul.getAttr("step", PyCallable.class);
+        this.pyClose = pysul.getAttr("close", PyCallable.class);
     }
 
-    /**
-     * Reset the Simulink model to the initial state.
-     */
-    public void reset() {
-        inputSignal = new Signal(signalStep);
-        PyCallable pyReset = this.pysul.getAttr("reset", PyCallable.class);
-        pyReset.call();
-        counter++;
+    public void pre() {
+        this.pyPre.call();
     }
 
-    /**
-     * Clear the counter and the time measure.
-     */
-    public void clear() {
-        simulationTime.reset();
-        counter = 0;
-    }
-    /*
-    public ArrayList<Double> origin() {
-        PyCallable pyOrigin = this.pysul.getAttr("origin", PyCallable.class);
-        ArrayList<Double> ret = pyOrigin.callAs(new ArrayList<Double>().getClass());
-        return ret;
-    }
-    */
-
-    /**
-     * Execute the Simulink model for one step by feeding inputSignal
-     * @param inputSignal The input signal
-     * @return The output signal with timestamps of the entire execution.
-     */
-    @Nonnull @SuppressWarnings("unchecked")
-    public List<Double> step(@Nonnull List<Double> inputSignal) throws Exception {
-        //System.out.println("Step");
-        this.inputSignal.add(inputSignal);
-        PyCallable pyStep = this.pysul.getAttr("step", PyCallable.class);
-
-        simulationTime.start();
-        ArrayList<Double> ret = pyStep.callAs(new ArrayList<Double>().getClass(), inputSignal);
-        simulationTime.stop();
-        
-        return ret;
+    public void post() {
+        this.pyPost.call();
     }
 
-
-    /**
-     * Execute the Simulink model by feeding inputSignal
-     * <p>
-     * For inputSignal = a1, a2, ..., an, we construct a timed word w = (a1, 0), (a2, T), (a3, 2 * T), ... (an, (n - 1) * T) and execute the Simulink model by feeding the piecewise-linear interpolation of w.
-     *
-     * @param inputSignal The input signal
-     * @return The output signal. The size is same as the input.
-     */
-    public ValueWithTime<List<Double>> execute(Word<List<Double>> inputSignal) throws Exception {
-        reset();
-
-        ArrayList<List<Double>> outputs = new ArrayList<List<Double>>();
-        ArrayList<Double> timestamps = new ArrayList<Double>();
-        //outputs.add(origin());
-        //timestamps.add(0.0);
-        for(var e : inputSignal)
-        {
-            outputs.add(step(e));
-            timestamps.add(this.getCurrentTime());
+    public ArrayList<Double> step(List<Double> inputSignal) throws JepException {
+        var ret = this.pyStep.callAs(ArrayList.class, inputSignal);
+        for (var e : ret) {
+            //Check if the each item in the returned list is double
+            if (!(e instanceof Double)) {
+                throw new JepException();
+            }
         }
-
-        return new ValueWithTime<>(timestamps, outputs);
+        return (ArrayList<Double>) ret;
     }
 
-    /**
-     * Close the MATLAB engine. This method must be called when the object is no longer used.
-     */
     public void close() {
-        PyCallable pyClose = this.pysul.getAttr("close", PyCallable.class);
-        pyClose.call();
+        this.pyClose.call();
     }
-
-    public double getSimulationTimeSecond() {
-        return this.simulationTime.getSecond();
-    }
-
 }
