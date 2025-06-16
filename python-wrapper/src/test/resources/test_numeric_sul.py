e39f593bf5e82172afa04fa5aaf95e7d20239d90
taiseiKMC <taisei@kmc.gr.jp>
2025-06-17 02:02:10 +0900

    Add PythonNumericSULTest

---
+++ python-wrapper/src/test/resources/test_numeric_sul.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class SUL:
+    def __init__(self):
+        self.state = (0.0, 0.0)
+
+    def step(self, inputSignal: List[float]) -> List[float]:
+        """
+        The first output is truncated to 100.0 if it exceeds 100.0.
+        The second output is the sum modulo 100.0.
+        """
+
+        [sum0, sum1] = self.state
+        sum0 = sum0 + sum(inputSignal)
+        sum0 = max(0.0, min(100.0, sum0))
+        sum1 = (sum1 + sum(inputSignal)) % 100.0
+        self.state = [sum0, sum1]
+        return self.state
+
+    def pre(self):
+        self.state = (0.0, 0.0)
+        
+    def post(self):
+        pass
+    def close(self):
+        pass
