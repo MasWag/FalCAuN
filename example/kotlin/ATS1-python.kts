3ac183839929d2dd8f03f4cb55dfa46a8fb6acab
taiseiKMC <taisei@kmc.gr.jp>
2025-06-19 04:28:05 +0900

    Add Python wrapper autotrans_matlab.py for Autotrans_shift Simulink model

---
+++ example/kotlin/ATS1-python.kts
@@ -0,0 +1,104 @@
+#!/usr/bin/env kscript
+
+var signalStep = 1.0
+val initScript = "./autotrans_matlab.py"
+
+// This script depends on FalCAuN-core and FalCAuN-python
+@file:DependsOn("net.maswag.falcaun:FalCAuN-core:1.0-SNAPSHOT", "net.maswag.falcaun:FalCAuN-python:1.0-SNAPSHOT")
+// And requires JEP library
+// Below is an example path to the JEP library when using pyenv and python 3.10.15
+//@file:KotlinOptions("-Djava.library.path=$PYENV_ROOT/versions/3.10.15/lib/python3.10/site-packages/jep")
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import net.automatalib.modelchecker.ltsmin.AbstractLTSmin
+import net.automatalib.modelchecker.ltsmin.LTSminVersion
+import net.maswag.falcaun.*
+import org.slf4j.LoggerFactory
+import kotlin.streams.toList
+
+// The following surprises the debug log
+var updaterLogger = LoggerFactory.getLogger(AbstractAdaptiveSTLUpdater::class.java) as Logger
+updaterLogger.level = Level.INFO
+var updateListLogger = LoggerFactory.getLogger(AdaptiveSTLList::class.java) as Logger
+updateListLogger.level = Level.INFO
+var LTSminVersionLogger = LoggerFactory.getLogger(LTSminVersion::class.java) as Logger
+LTSminVersionLogger.level = Level.INFO
+var AbstractLTSminLogger = LoggerFactory.getLogger(AbstractLTSmin::class.java) as Logger
+AbstractLTSminLogger.level = Level.INFO
+var EQSearchProblemLogger = LoggerFactory.getLogger(EQSearchProblem::class.java) as Logger
+EQSearchProblemLogger.level = Level.INFO
+var SimulinkSteadyStateGeneticAlgorithmLogger = LoggerFactory.getLogger(EQSteadyStateGeneticAlgorithm::class.java) as Logger
+SimulinkSteadyStateGeneticAlgorithmLogger.level = Level.INFO
+
+// Define the input and output mappers
+val throttleValues = listOf(0.0, 100.0)
+val brakeValues = listOf(0.0, 325.0)
+val inputMapper = InputMapperReader.make(listOf(throttleValues, brakeValues))
+val velocityValues = listOf(20.0, 40.0, 60.0, 80.0, 100.0, 120.0, null)
+val accelerationValues = listOf(null)
+val gearValues = listOf(null)
+val outputMapperReader = OutputMapperReader(listOf(velocityValues, accelerationValues, gearValues))
+outputMapperReader.parse()
+val signalMapper = ExtendedSignalMapper()
+val mapper =
+    NumericSULMapper(inputMapper, outputMapperReader.largest, outputMapperReader.outputMapper, signalMapper)
+
+// Define the STL properties
+val stlFactory = STLFactory()
+val stlList = listOf(
+    "[] (signal(0) < 120)",
+    "[] (signal(0) < 100)",
+    "[] (signal(0) < 80)",
+    "[] (signal(0) < 60)",
+    "[] (signal(0) < 40)",
+    "[] (signal(0) < 20)"
+).stream().map { stlString ->
+    stlFactory.parse(
+        stlString,
+        inputMapper,
+        outputMapperReader.outputMapper,
+        outputMapperReader.largest
+    )
+}.toList()
+val signalLength = 30
+val properties = AdaptiveSTLList(stlList, signalLength)
+
+// Constants for the GA-based equivalence testing
+val maxTest = 50000
+val populationSize = 200
+val crossoverProb = 0.5
+val mutationProb = 0.01
+
+// Load the automatic transmission model. This automatically closes MATLAB
+PythonContinuousNumericSUL(initScript, signalStep).use { autoTransSUL ->
+    // Configure and run the verifier
+    val verifier = NumericSULVerifier(autoTransSUL, signalStep, properties, mapper)
+    // Timeout must be set before adding equivalence testing
+    verifier.setTimeout(5 * 60 * 8) // 5 minutes
+    verifier.addCornerCaseEQOracle(signalLength, signalLength / 2);
+    verifier.addGAEQOracleAll(
+        signalLength,
+        maxTest,
+        ArgParser.GASelectionKind.Tournament,
+        populationSize,
+        crossoverProb,
+        mutationProb
+    )
+    val result = verifier.run()
+
+    // Print the result
+    if (result) {
+        println("The property is likely satisfied")
+    } else {
+        for (i in 0 until verifier.cexProperty.size) {
+            println("${verifier.cexProperty[i]} is falsified by the following counterexample")
+            println("cex concrete input: ${verifier.cexConcreteInput[i]}")
+            println("cex abstract input: ${verifier.cexAbstractInput[i]}")
+            println("cex output: ${verifier.cexOutput[i]}")
+        }
+    }
+    println("Execution time for simulation: ${verifier.simulationTimeSecond} [sec]")
+    println("Number of simulations: ${verifier.simulinkCount}")
+    println("Number of simulations for equivalence testing: ${verifier.simulinkCountForEqTest}")
+}
