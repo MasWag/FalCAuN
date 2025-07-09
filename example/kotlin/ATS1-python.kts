aadb3818e71b090eaa1a7626f758356772e0170c
taiseiKMC <taisei@kmc.gr.jp>
2025-07-10 01:36:21 +0900

    Fix a comment in ATS1-python.kts

--- example/kotlin/ATS1-python.kts
+++ example/kotlin/ATS1-python.kts
@@ -75,7 +75,7 @@ PythonContinuousNumericSUL(initScript, signalStep).use { autoTransSUL ->
     // Configure and run the verifier
     val verifier = NumericSULVerifier(autoTransSUL, signalStep, properties, mapper)
     // Timeout must be set before adding equivalence testing
-    verifier.setTimeout(5 * 60 * 8) // 5 minutes
+    verifier.setTimeout(60 * 40) // 40 minutes
     verifier.addCornerCaseEQOracle(signalLength, signalLength / 2);
     verifier.addGAEQOracleAll(
         signalLength,
