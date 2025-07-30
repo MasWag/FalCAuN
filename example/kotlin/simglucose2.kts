bb56b5cf14c875db9e0be85774f6c16123025b9c
taiseiKMC <taisei@kmc.gr.jp>
2025-07-31 02:03:25 +0900

    Update imports in example/kotlin

--- example/kotlin/simglucose2.kts
+++ example/kotlin/simglucose2.kts
@@ -14,6 +14,9 @@ import ch.qos.logback.classic.Logger
 import net.automatalib.modelchecker.ltsmin.AbstractLTSmin
 import net.automatalib.modelchecker.ltsmin.LTSminVersion
 import net.maswag.falcaun.*
+import net.maswag.falcaun.parser.ExtendedSignalMapper
+import net.maswag.falcaun.parser.STLFactory
+import net.maswag.falcaun.python.PythonNumericSUL
 import org.slf4j.LoggerFactory
 import kotlin.streams.toList
 import net.automatalib.word.Word;
