3fd40ebe92374adb855a81613882d6108d318c51
Masaki Waga <masakiwaga@gmail.com>
2026-01-01 15:44:41 +0900

    Add default pre/post composition methods to ComponentWiseSignalDiscretizer
    This change introduces SULMapper imports and a @NonNull annotation, then adds two new default methods:
    - `preCompose` – returns a `PreComposedSignalDiscretizer` composed with a provided `SULMapper`.
    - `postCompose` – returns a `PostComposedSignalDiscretizer` composed with a provided `SULMapper`.
    
    These methods simplify creating composed discretizers directly from an existing `ComponentWiseSignalDiscretizer`.

--- core/src/main/java/net/maswag/falcaun/ComponentWiseSignalDiscretizer.java
+++ core/src/main/java/net/maswag/falcaun/ComponentWiseSignalDiscretizer.java
@@ -1,5 +1,8 @@
 package net.maswag.falcaun;
 
+import de.learnlib.sul.SULMapper;
+import lombok.NonNull;
+
 import java.util.List;
 
 public interface ComponentWiseSignalDiscretizer extends SignalDiscretizer {
@@ -12,4 +15,18 @@ public interface ComponentWiseSignalDiscretizer extends SignalDiscretizer {
      * @return The list of possible abstract output characters for each component
      */
     List<List<Character>> getAbstractOutputs();
+
+    /**
+     * SignalDiscretizer pre-composed with an SULMapper.
+     */
+    default ComponentWiseSignalDiscretizer preCompose(@NonNull SULMapper<List<Double>, IOSignalPiece<List<Double>>, List<Double>, IOSignalPiece<List<Double>>> preMapper) {
+        return new PreComposedSignalDiscretizer(this, preMapper);
+    }
+
+    /**
+     * SignalDiscretizer post-composed with an SULMapper.
+     */
+    default SignalDiscretizer postCompose(@NonNull SULMapper<String, String, String, String> postMapper) {
+        return new PostComposedSignalDiscretizer(this, postMapper);
+    }
 }
