23bebf2659149d353a75310d27b3f2a96dc1298a
Masaki Waga <masakiwaga@gmail.com>
2026-01-22 10:25:46 +0900

    Fixed path

--- matlab/src/main/java/net/maswag/falcaun/simulink/SimulinkModel.java
+++ matlab/src/main/java/net/maswag/falcaun/simulink/SimulinkModel.java
@@ -18,6 +18,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Stream;
 import java.util.concurrent.ExecutionException;
 
 /**
